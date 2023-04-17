0233d03ed71c2a180be9d7c2437dbc16080b02b5
Dell <Dell@Krutik>
2023-04-17 13:42:23 +0530

    day five commit`

---
+++ Dayfive/src/Superkeyword/Animal.java
@@ -0,0 +1,19 @@
+package Superkeyword;
+
+public class Animal {
+	public String animalType;
+	public int noLegs;
+	
+	public void display()
+	{
+		System.out.println("Animal Type"+animalType);
+		System.out.println("No of Legs"+noLegs);
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [animalType=" + animalType + ", noLegs=" + noLegs + "]";
+	}
+
+
+}
---
+++ Dayfive/src/Superkeyword/SuperExecutor.java
@@ -0,0 +1,11 @@
+package Superkeyword;
+
+public class SuperExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tiger t=new Tiger();
+		t.display();
+	}
+
+}
