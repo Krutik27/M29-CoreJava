4d909f561943008f32d547cb41f372510ec674ee
Dell <Dell@Krutik>
2023-04-17 12:54:26 +0530

    Day five commit

---
+++ Dayfive/src/multilevel/MultilevelInheritance.java
@@ -0,0 +1,16 @@
+package multilevel;
+
+public class MultilevelInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		City c=new City();
+		c.setCityName("Boisar");
+		c.setArea("Bhimnagar");
+		c.setStateName("Maharashtra");
+		c.setCountryName("India");
+		c.setCountryCapital("Delhi");
+		System.out.println(c);
+	}
+
+}
---
+++ Dayfive/src/singleinheritance/SingleInheritance.java
@@ -0,0 +1,11 @@
+package singleinheritance;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student("Niket","134568923","Mumbai",9922334455L,"SJCEM",16);
+		System.out.println(s);
+	}
+
+}
